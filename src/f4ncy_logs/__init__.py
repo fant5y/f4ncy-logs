478e4d198e9539fc4d27c611a7822427b52abc75
fant5y <vroni@salala.de>
2026-08-27 20:36:28 +0200

    ✨: export `f4ncy_log` from the package root
    - Add `f4ncy_log` to the public imports in `src/f4ncy_logs/__init__.py`.
    
    Signed-off-by: fant5y <vroni@salala.de>

--- src/f4ncy_logs/__init__.py
+++ src/f4ncy_logs/__init__.py
@@ -1,3 +1,6 @@
-from f4ncy_logs.config import f4ncy_print as f4ncy_print, get_logger as get_logger, rtk as f4ncy_rtk
+from f4ncy_logs.config import f4ncy_log as f4ncy_log,\
+    f4ncy_print as f4ncy_print,\
+    get_logger as get_logger,\
+    rtk as f4ncy_rtk
 
 rtk = next(f4ncy_rtk())
